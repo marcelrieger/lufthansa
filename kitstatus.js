da5c00f7f796cb1a4aba171f3fa59e03607cdf80
Marcel Rieger <marcel@blim.us>
2015-07-30 15:04:44 +0200

    alpha-7.6.1:Breadcrumbs: new styling

--- kitstatus.js
+++ kitstatus.js
@@ -155,10 +155,12 @@ function sync_kits( ) {
 		}
 
 		if (status2) {
+			console.log("STATUS2")
 			$( ".breadcrumb" ).children().children().eq(0).removeClass("animate");
 			$( ".breadcrumb" ).children().children().eq(1).addClass("active").addClass("animate");
 			$( ".breadcrumb" ).children().children().eq(2).removeClass("animate").removeClass("active");
 		} else {
+			console.log("STATUS2OFF")
 			$( ".breadcrumb" ).children().children().eq(1).removeClass("animate").removeClass("active");
 			$( ".breadcrumb" ).children().children().eq(0).addClass("animate");
 		}
