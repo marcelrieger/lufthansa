8758ab2fd547fa2e6dd4d51cadcf250f4734c4b6
Marcel Rieger <marcel@blim.us>
2015-07-30 16:23:37 +0200

    alpha-7.8.3:Breadcrumbs: animation added

--- kitstatus.js
+++ kitstatus.js
@@ -148,15 +148,19 @@ function sync_kits( ) {
 
 			if (status3) {
 				statusf = 2;
+				$( ".breadcrumb" ).children().children().eq(0).addClass("active").removeClass("animate");
 				$( ".breadcrumb" ).children().children().eq(1).addClass("active").removeClass("animate");
 				$( ".breadcrumb" ).children().children().eq(2).addClass("active").addClass("animate");
 			} else if (status2) {
 				statusf = 1;
+
 				$( ".breadcrumb" ).children().children().eq(2).removeClass("active").removeClass("animate");
 				$( ".breadcrumb" ).children().children().eq(1).addClass("active").addClass("animate");
+				$( ".breadcrumb" ).children().children().eq(0).addClass("active").removeClass("animate");
 			} else {
 				$( ".breadcrumb" ).children().children().eq(2).removeClass("active").removeClass("animate");
 				$( ".breadcrumb" ).children().children().eq(1).removeClass("active").removeClass("animate");
+				$( ".breadcrumb" ).children().children().eq(0).addClass("active").addClass("animate");
 			}
 
 			request_obj = {
