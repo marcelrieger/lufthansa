8ecc667b6f07b55a2b7d42f779b6d851668d0332
Marcel Rieger <marcel@blim.us>
2015-07-30 15:15:55 +0200

    alpha-7.7:Initialization page

--- include/page_initialize.php
+++ include/page_initialize.php
@@ -1,5 +1,21 @@
-<h2>Initialisierung</h2>
-<form action="/?scenario=init" method="post">
-<textarea name="code" cols="128" rows="32"></textarea>
-<input type="submit" />
-</form>
+<div class="pagetitle">
+  <div class="container">
+    <div class="title">
+      Initialisierung
+    </div>
+  </div>
+</div>
+
+<div class="container">
+
+  <div class="pagecontent">
+
+    <form action="/?scenario=init" method="post">
+
+    <div class="form-group">
+      <textarea name="code" cols="128" rows="32"></textarea>
+    </div>
+    <button type="submit" class="btn btn-default">Senden</button>
+    </form>
+
+  </div></div>
