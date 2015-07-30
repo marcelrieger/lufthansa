665d88851e2270e9a185d52c2db481db0ed42f6a
Marcel Rieger <marcel@blim.us>
2015-07-30 14:08:51 +0200

    alpha-6.8.9: Update function updated

--- include/page_kit.php
+++ include/page_kit.php
@@ -18,7 +18,7 @@ current_kit_id = <?php echo $current_kit[ "id" ]; ?>;
 <div class="pagetitle">
   <div class="container">
     <div class="title">
-			<a href="/?scenario=kitting">Auftragsübersicht</a> &gt; <a href="/?scenario=kitting&order_id=<?php echo $current_order[ "id" ]; ?>">Auftrag  <?php echo $current_order[ "id" ]; ?></a> &gt; Kit <?php echo ($current_kit[ "id" ]+1); ?>
+			<a href="/?scenario=kitting">Auftragsübersicht</a> &gt; <a href="/?scenario=kitting&order_id=<?php echo $current_order[ "id" ]; ?>">Auftrag  <?php echo $current_order[ "deadline" ]; ?></a> &gt; Kit <?php echo ($current_kit[ "id" ]+1); ?>
     </div>
 		<?php
 
