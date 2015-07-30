3b00ebbaaa829e04dea8319e7857415b27be4d2c
Marcel Rieger <marcel@blim.us>
2015-07-30 15:11:26 +0200

    alpha-7.6.1:Breadcrumbs: new styling

--- include/page_overview.php
+++ include/page_overview.php
@@ -5,7 +5,7 @@
     <div class="menu">
       <a href="/?scenario=kitting" class="menubtn">Kitting</a>
       <a href="/?scenario=storage" class="menubtn">Mannloses Lager</a>
-      <a href="/?scenario=init" class="menubtn">Initialisierung (Nur Administrator)</a>
+      <?php if ($role_id==LTLS_ADMIN) { ?><a href="/?scenario=init" class="menubtn">Initialisierung (Nur Administrator)</a><?php } ?>
     </div>
 
   </div>
