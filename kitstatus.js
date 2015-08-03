ea3d5fa2c2830686d2abf2b819203c1e6f74d51b
Marcel Rieger <marcel@blim.us>
2015-08-03 16:44:28 +0200

    Print function

--- kitstatus.js
+++ kitstatus.js
@@ -181,7 +181,7 @@ function printcertificate( ) {
 }
 
 $(document).on('click', '.printcertificate', function () {
-	$(".message").append("<div class=\"alert alert-success\" role=\"alert\"><a href=\"#\" class=\"alert-link\">Das Zertifikat wurde ausgedruckt</a></div>");
+	$(".message").html("<div class=\"alert alert-info\" role=\"alert\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"><span aria-hidden="true">&times;</span></button>Das Zertifikat wurde ausgedruckt.</div>").hide().fadeIn("fast");
 	//printcertificate();
 });
 
