1094d650471c7fde2f3b74ab4e474de4c8690c99
Marcel Rieger <marcel@blim.us>
2015-08-11 11:23:06 +0200

    LED Controller

--- scripts/led.py
+++ scripts/led.py
@@ -68,7 +68,7 @@ def opr2(ls):
 
     if len(sys.argv)>2:
         for i in range (2, len(sys.argv)):
-            toggleLED(ls, int(sys.argv[1]), sys.argv[i])
+            toggleLED(ls, 1, sys.argv[i])
 
 if __name__ == "__main__":
 
