7870fa156c59c499e3e1f0ff84a8a883dda6e733
Marcel Rieger <marcel@blim.us>
2015-08-11 14:21:39 +0200

    LED Controller

--- scripts/a.py
+++ scripts/a.py
@@ -1,25 +1,93 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-import threading
-import time
+HOST = "137.226.151.148"
+PORT = 4223
 
-exitFlag = 0
+import sys, time
+from tinkerforge.ip_connection import IPConnection
+from tinkerforge.bricklet_led_strip import LEDStrip
 
-class myThread (threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
+locList = [
+    [
+        0, 15, 20, 35, 40, 55, 60, 75, 80
+    ],
+    [
+        5, 10, 25, 30, 45, 50, 65, 70, 85
+    ]
+]
 
-    def run(self):
-        print "Starting "
-        time.sleep(2)
-        print "Exiting "
+def disconnect():
+    #raw_input()
+    #time.sleep(2)
+    ipcon.disconnect()
 
+def turnoff(ls, delay=0):
 
-# Create new threads
-thread1 = myThread()
+    if delay:
+        time.sleep(delay)
+    for i in range(0,20):
+        ls.set_rgb_values(i*10, 10, [0]*16, [0]*16, [0]*16)
 
-# Start new Threads
-thread1.start()
+def toggleLED(ls,state,position=None):
 
+    if position:
+        print(position+": "+str(parsePosition(str(position))))
+        position = parsePosition(str(position))
+    else:
+        turnoff(ls)
+        return
 
-print "Exiting Main Thread"
+    if state==3:
+        turnoff(ls)
+    elif state:
+        ls.set_rgb_values(int(position), 5, [0]*16, [179]*16, [255]*16)
+    else:
+        ls.set_rgb_values(int(position), 5, [0]*16, [0]*16, [0]*16)
+
+def parsePosition(position):
+    col = position[3:]
+    row = position[:2]
+    if col == 'A':
+        col = 0
+    else:
+        col = 1
+    return locList[col][int(row)-1]
+
+def opr(ls):
+
+    # Newly added location will be stacked together with the old location data
+
+    if sys.argv == 3:
+        turnoff(ls,0.5)
+        turnoff(ls,1)
+        return
+
+    if len(sys.argv)<3:
+        toggleLED(ls, int(sys.argv[1]))
+    else:
+        for i in range (2, len(sys.argv)):
+            toggleLED(ls, int(sys.argv[1]), sys.argv[i])
+
+def opr2(ls):
+
+    # Newly added location will rewrite all previous location data
+
+    toggleLED(ls, int(sys.argv[1]))
+    if len(sys.argv)>2:
+        for i in range (2, len(sys.argv)):
+            toggleLED(ls, int(sys.argv[1]), sys.argv[i])
+
+if __name__ == "__main__":
+
+    ipcon = IPConnection()
+    ipcon.connect(HOST, PORT)
+    ipcon.set_timeout(10000)
+    ipcon.wait()
+
+    ls = LEDStrip("oV7", ipcon)
+
+    opr2(ls)
+
+    ls.register_callback(ls.CALLBACK_FRAME_RENDERED, disconnect())
+    #disconnect()
